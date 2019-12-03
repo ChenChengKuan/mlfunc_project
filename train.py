3062cc0b838d3b310e61f95db5d7c2669d730deb
ChenChengKuan <sttagomantis@gmail.com>
2019-12-03 13:11:32 +0800

    fix bugs

--- train.py
+++ train.py
@@ -4,6 +4,7 @@ import copy
 import argparse
 import torch
 import os
+import json
 import torch.nn as nn
 import numpy as np
 from torch.utils.data import DataLoader
@@ -23,6 +24,7 @@ if __name__ == '__main__':
     parser.add_argument('--batch_size', type=int, default=128)
     parser.add_argument('--num_gene_selected', type=int, default=3000)
     parser.add_argument('--paired_data', default=False)
+    parser.add_argument('--save_cell_map', default=False, action='store_true')
 
     parser.add_argument('--learning_rate', type=float, default=1e-3)
     parser.add_argument('--sgd_momentum', type=float, default=0.9)
@@ -42,10 +44,12 @@ if __name__ == '__main__':
     device = torch.device('cuda')
 
     if not args.paired_data:
-        data_name = "_".join(os.path.split(args.dataset_train)[-1].split("_")[0:-1])
+        results_path = os.path.join(args.save_path, os.path.split(args.dataset_train)[-1].split(".")[0])
     else:
-        data_name = "_".join(os.path.split(args.dataset_train)[-1].split("_")[0:-1] +
-                             os.path.split(args.dataset_test)[-1].split("_")[0:-1])
+        results_path = os.path.join(args.save_path, "_".join([os.path.split(args.dataset_train)[-1].split(".")[0],\
+                                                           os.path.split(args.dataset_test)[-1].split(".")[0]]))
+    if not os.path.exists(results_path):
+        os.makedirs(results_path, exist_ok=True)
 
     sc_dataset_train = SingleCellData(data_path=args.dataset_train, num_gene=args.num_gene_selected)
 
@@ -53,7 +57,10 @@ if __name__ == '__main__':
                                      num_gene=args.num_gene_selected,\
                                      shared_gene_mask=sc_dataset_train.gene_mask,\
                                      filter_mask = sc_dataset_train.filter_mask)
-    
+    if args.save_cell_map:
+        with open(os.path.join(results_path, 'id2cell.json'), 'w') as handle:
+            json.dump(sc_dataset_train.id_to_cell, handle)
+    print(len(sc_dataset_train), len(sc_dataset_test))    
     assert(np.array_equal(sc_dataset_train.gene_mask, sc_dataset_test.gene_mask))
     assert(np.array_equal(sc_dataset_train.filter_mask, sc_dataset_test.filter_mask))
 
@@ -82,10 +89,7 @@ if __name__ == '__main__':
         model_name = 'MLP_{}'.format("-".join(args.layers))
     
     optimizer = torch.optim.SGD(parameters, lr=args.learning_rate, momentum=args.sgd_momentum)
-    results_path = os.path.join(args.save_path, data_name)
 
-    if not os.path.exists(args.save_path):
-        os.mkdir(args.save_path)
 
     embedding_writer = SummaryWriter(results_path)
 
