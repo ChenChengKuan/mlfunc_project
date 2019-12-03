2b6005aedf0bcf31e717e1292e357b7e5692734d
ChenChengKuan <sttagomantis@gmail.com>
2019-12-03 12:46:38 +0800

    update sample and add run_ref

---
+++ sample.sh
@@ -0,0 +1,6 @@
+python sample.py --ref_file_name ./data/* --output_path ./data_sample/ --sample_rate 0.05
+python sample.py --ref_file_name ./data/* --output_path ./data_sample/ --sample_rate 0.15
+python sample.py --ref_file_name ./data/* --output_path ./data_sample/ --sample_rate 0.25
+python sample.py --ref_file_name ./data/* --output_path ./data_sample/ --sample_rate 0.99
+
+
